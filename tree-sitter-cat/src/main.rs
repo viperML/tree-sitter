8f9b16fdc52d445758014d41231d835b070c9669
Fernando Ayats <ayatsfer@gmail.com>
2024-07-22 23:12:57 +0200

    test html

--- tree-sitter-cat/src/main.rs
+++ tree-sitter-cat/src/main.rs
@@ -1,68 +1,71 @@
 use std::io::{stdout, Write};
 
 use tree_sitter_dynamic::DynTS;
-use tree_sitter_highlight::{HighlightEvent, Highlighter};
+use tree_sitter_highlight::{Highlight, HighlightEvent, Highlighter};
+use itertools::Itertools;
 
 fn main() -> eyre::Result<()> {
-    let js = unsafe {
-        DynTS::new(
-            "javascript",
-            &[
-                "attribute",
-                "constant",
-                "function.builtin",
-                "function",
-                "keyword",
-                "operator",
-                "property",
-                "punctuation",
-                "punctuation.bracket",
-                "punctuation.delimiter",
-                "string",
-                "string.special",
-                "tag",
-                "type",
-                "type.builtin",
-                "variable",
-                "variable.builtin",
-                "variable.parameter",
-            ],
-        )?
-    };
+    let names = [
+        "attribute",
+        "constant",
+        "function.builtin",
+        "function",
+        "keyword",
+        "operator",
+        "property",
+        "punctuation",
+        "punctuation.bracket",
+        "punctuation.delimiter",
+        "string",
+        "string.special",
+        "tag",
+        "type",
+        "type.builtin",
+        "variable",
+        "variable.builtin",
+        "variable.parameter",
+    ];
+
+    let js = unsafe { DynTS::new("javascript", &names)? };
 
     let mut highlighter = Highlighter::new();
 
-    let s =  b"const foo = bar;";
+    let s = br#"import { getCollection } from "astro:content";
+import Post from "./Post.astro";
 
-    let highlights =
-        highlighter.highlight(js.highlight_config(), b"const foo = bar;", None, |_| None)?;
+const blogEntries = (await getCollection("blog"))
+    .sort((a, b) => {
+        return b.data.pubDate.getTime() - a.data.pubDate.getTime();
+    })
+    .filter((elem) => {
+        if (elem.data.draft === true) {
+            return false;
+        } else {
+            return true;
+        }
+    });"#;
 
+    let highlights = highlighter.highlight(js.highlight_config(), s, None, |_| None)?;
 
     let mut color = false;
+    print!("<pre>");
     for event in highlights {
         match event.unwrap() {
             HighlightEvent::Source { start, end } => {
-                // eprintln!("source: {}-{}", start, end);
                 for i in start..end {
-                    stdout().write_all(&[ s[i] ])?;
+                    stdout().write_all(&[s[i]])?;
                 }
-
             }
-            HighlightEvent::HighlightStart(s) => {
-                // eprintln!("highlight style started: {:?}", s);
-                if color {
-                    print!("\x1b[31m");
-                    color = false;
-                } else {
-                    print!("\x1b[0m");
-                    color = true;
-                }
+            HighlightEvent::HighlightStart(Highlight(n)) => {
+                let name = names[n].split(".").join(" ");
+                print!("<span class=\"{}\">", name);
             }
             HighlightEvent::HighlightEnd => {
-                // eprintln!("highlight style ended");
+                print!("</span>")
             }
         }
     }
+    print!("</pre>");
 
     Ok(())
 }
