e7651cefe80ffbdd25a073c9448f41ee3ad5c66c
Fernando Ayats <ayatsfer@gmail.com>
2024-08-21 22:52:42 +0200

    tweak cats

--- tree-sitter-cat/src/main.rs
+++ tree-sitter-cat/src/main.rs
@@ -1,31 +1,33 @@
 use core::str;
 use std::{
     collections::HashMap,
-    fs::{self},
-    path::PathBuf,
+    fs::{self, File},
+    io::{self, stdout, BufReader, Write},
+    path::{Path, PathBuf},
 };
 
 use clap::Parser;
+use is_terminal::IsTerminal;
 use serde::Deserialize;
 use tree_sitter_dynamic::{DynTS, STANDARD_CAPTURE_NAMES};
 use tree_sitter_highlight::{Highlight, HighlightEvent};
 
 #[derive(Debug, Parser)]
-/// treesitter-powered cat clone
+/// Tree-sitter-powered cat clone
 struct Args {
     /// File to colorize
     file: PathBuf,
 
-    /// Which language the source code is written in
+    /// Language to use
     #[arg(short, long)]
     language: Option<String>,
 
-    #[arg(long, short)]
     /// Output information about the tree-sitter highlights
+    #[arg(long, short)]
     debug: bool,
 
-    #[arg(short, long)]
     /// Path to an alternative stylesheet
+    #[arg(short, long)]
     stylesheet: Option<PathBuf>,
 }
 
@@ -38,16 +40,50 @@ struct StyleSheet {
     theme: HashMap<String, String>,
 }
 
+fn regular_cat<P>(path: P) -> eyre::Result<()>
+where
+    P: AsRef<Path>,
+{
+    let p = path.as_ref();
+
+    let mut reader = BufReader::new(File::open(p)?);
+    io::copy(&mut reader, &mut stdout())?;
+    return Ok(());
+}
+
 fn main() -> eyre::Result<()> {
-    color_eyre::install()?;
+    color_eyre::config::HookBuilder::default()
+        .display_env_section(false)
+        .install()?;
+
     let args = Args::parse();
+
+    if !stdout().is_terminal() {
+        regular_cat(&args.file)?;
+        return Ok(());
+    }
+
+    let cookie = magic::Cookie::open(magic::cookie::Flags::MIME_ENCODING)?;
+    let database = Default::default();
+    let cookie = cookie.load(&database).unwrap();
+    let file_result = cookie.file(&args.file)?;
+
+    if file_result == "binary" {
+        return Err(eyre::eyre!("Reading binary file, refusing to operate"));
+    }
 
     let language = match args.language {
         Some(l) => l,
-        None => tree_sitter_dynamic::detect_language(
+        None => match tree_sitter_dynamic::detect_language(
             std::env::var("TS_GRAMMAR_PATH")?,
             args.file.as_path(),
-        )?,
+        ) {
+            Ok(ok) => ok,
+            Err(_) => {
+                regular_cat(&args.file)?;
+                return Ok(());
+            }
+        },
     };
 
     let mut ts = DynTS::new(language, STANDARD_CAPTURE_NAMES)?;
@@ -61,15 +97,6 @@ fn main() -> eyre::Result<()> {
 
     let prefix = &stylesheet.prefix.unwrap_or_default();
     let end = &stylesheet.end.unwrap_or_default();
-
-    let cookie = magic::Cookie::open(magic::cookie::Flags::MIME_ENCODING)?;
-    let database = Default::default();
-    let cookie = cookie.load(&database).unwrap();
-    let file_result = cookie.file(&args.file)?;
-
-    if file_result == "binary" {
-        return Err(eyre::eyre!("Reading binary file, refusing to operate"));
-    }
 
     for event in ts.highlight(&contents) {
         match event.unwrap() {
